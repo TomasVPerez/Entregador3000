23774776ada30263971c5f3983117f030ae31d39
Tomás <80177664+TomasVPerez@users.noreply.github.com>
2021-03-12 08:46:07 -0300

    Add files via upload

---
+++ Entregador3000.py
@@ -0,0 +1,68 @@
+import os, time
+from docx import Document
+from docx.shared import Inches, Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.style import WD_STYLE_TYPE
+
+class Sistema:
+    def __init__(self):
+        self.extensiones = ["jpg", "PNG", "jpeg"]
+        self.imagenes = []
+
+        self.usuario = usuario = os.path.expanduser("~")
+        self.descargas = descargas = f"{usuario}/Downloads"
+
+    def mostrarMenu(self):
+        print('''
+    ______      __                            __              _____ ____  ____  ____ 
+   / ____/___  / /_________  ____ _____ _____/ /___  _____   |__  // __ \/ __ \/ __ \\
+  / __/ / __ \/ __/ ___/ _ \/ __ `/ __ `/ __  / __ \/ ___/    /_ </ / / / / / / / / /
+ / /___/ / / / /_/ /  /  __/ /_/ / /_/ / /_/ / /_/ / /      ___/ / /_/ / /_/ / /_/ / 
+/_____/_/ /_/\__/_/   \___/\__, /\__,_/\__,_/\____/_/      /____/\____/\____/\____/  
+                          /____/         
+by: Tomas Perez                                            
+''')
+        nombreEntrega = input("Nombre del entregable: ")
+        header = input("Materia-NombreyApellido(nroAlumno)-Fecha: ")
+        self.crearDocumento(nombreEntrega, header)
+        
+    def guardarImagenes(self):
+        archivos = next(os.walk(self.descargas))[2]
+        for archivo in archivos:
+            if "WhatsApp" in archivo and archivo.split(".")[-1] in self.extensiones:
+                self.imagenes.append(archivo)
+                # print(self.imagenes)
+
+    def crearDocumento(self, nombre, header):
+        word = Document()
+        encabezado = word.sections[0].header
+        encabezado.paragraphs[0].text = header
+        estilo = word.styles["Normal"]
+        fuente = estilo.font
+        fuente.name = "Arial"
+        fuente.size = Pt(11)
+        for imagen in self.imagenes:
+            creacion = time.ctime(os.path.getctime(f"{self.descargas}/{imagen}"))
+            if time.ctime()[:13] == creacion[:13]: #comparo dia mes y hora
+                word.add_picture(f"{self.descargas}/{imagen}", width=Inches(5.0))
+                ultimaFoto = word.paragraphs[-1] 
+                ultimaFoto.alignment = WD_ALIGN_PARAGRAPH.CENTER
+            else:
+                pass
+        nuevaCarpeta = f"{self.usuario}/Desktop/{nombre}"
+        os.mkdir(nuevaCarpeta)
+        word.save(f"{nuevaCarpeta}/{nombre}.docx")
+        print("Documento creado en el escritorio")
+        time.sleep(1)       
+
+
+def main():
+    s = Sistema()
+    s.guardarImagenes()
+    s.mostrarMenu()
+
+if __name__ == '__main__':
+    main()
+
+
+
